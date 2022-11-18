c2fadb44db9b859586d39761a5569b12a3addcac
Pedro Cruz <cruzrodriguezpedroj@gmail.com>
2022-11-18 16:12:19 -0600

    backend: se crea estructura para modelo sala de juntas y se crea estructura para su esquema y su clase, se instalan dependencias para postgres

---
+++ backendExpress/db/models/index.js
@@ -0,0 +1,10 @@
+//se importa la clase sala y el esquema  de su modelo
+const { Sala, SalaSchema} = require('./sala.model');
+
+//funcion para inicializar los esquemas de los modelos
+function setupModels (){
+
+}
+
+//se exportan los modelos creados
+module.exports = setupModels;
---
+++ backendExpress/db/models/sala.model.js
@@ -0,0 +1,13 @@
+
+//esquema que tendra  una sala de juntas en su tabla, aqui iran los campos que fgromaran la tabla sala de juntas
+const SalaSchema = {
+
+}
+
+//la clase sala  estendera de un modelo
+class Sala extends Model{
+
+}
+
+//se exporta el schema  y la clase para su uso
+module.exports = {SalaSchema, Sala};
