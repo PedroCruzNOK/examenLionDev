b1253e032f5fe7194d0313b96bf7bba13e7478c5
Pedro Cruz <cruzrodriguezpedroj@gmail.com>
2022-11-18 13:59:32 -0600

    backedn: se crea el end point raiz en index y la carpeta de rutas para los diferentes controllers

--- backendExpress/index.js
+++ backendExpress/index.js
@@ -1,1 +1,23 @@
-console.log('backend prueba');
+//importamos librerias para arrancar servicios
+const express = require('express');
+const cors = require ('cors');
+const routerApi = require('./routes');
+
+const app = express();
+const port = 5000;
+
+app.use(express.json());
+app.use(cors());
+
+//end point inicial raiz
+app.get('/', (req, res) => {
+  res.send('hola express');
+
+})
+
+routerApi(app);
+
+//Escucha de nuestro servidor, se manda puerto
+app.listen(port, () => {
+  console.log('hola puerto' + port);
+});
---
+++ backendExpress/routes/index.js
@@ -0,0 +1,6 @@
+//funcion para esportar a el index de nuestra aplicacion y donde enviaremos el
+//arreglo de  end point´s
+function routerApi(app){
+}
+
+module.exports = routerApi
