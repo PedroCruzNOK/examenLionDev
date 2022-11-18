c566e033bfded4e754a2407c67d9e057faa6cc24
Pedro Cruz <cruzrodriguezpedroj@gmail.com>
2022-11-18 16:35:38 -0600

    backend: estructura para la conexion a la base de datos, carpeta lib para la conexion  e integracion con el moidelo salas

---
+++ backendExpress/libs/sequelize.js
@@ -0,0 +1,8 @@
+//importacion de dependencias para  la conexion a la base de datos y a los modelos
+const { Sequelize } = require ('sequelize');
+const {config} = require ('../config/config');
+const setupModels = require('../db/models/index')
+
+
+
+module.exports = sequelize;
