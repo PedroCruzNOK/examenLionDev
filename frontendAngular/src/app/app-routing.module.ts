960a1472100628fdb1299344dfbcadef0e9fd88c
Pedro Cruz <cruzrodriguezpedroj@gmail.com>
2022-11-22 13:07:48 -0600

    frontend: se trabaja en el routeo  para agregar y editar

---
+++ frontendAngular/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AddEditSalaComponent } from './components/add-edit-sala/add-edit-sala.component';
+//importamos nuestro componente de listas de salas
+import { ListSalasComponent } from './components/list-salas/list-salas.component';
+
+const routes: Routes = [
+  //agregamos la ruta para el listado de salas
+  {path: '', component: ListSalasComponent},
+  //agregamos la ruta para agregar una sala
+  {path: 'agregar', component: AddEditSalaComponent},
+  {path: '**', redirectTo: '', pathMatch:'full' }
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
--- frontendAngular/src/app/app.module.ts
+++ frontendAngular/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ListSalasComponent } from './components/list-salas/list-salas.component';
@@ -14,7 +14,8 @@ import { AddEditSalaComponent } from './components/add-edit-sala/add-edit-sala.c
     AddEditSalaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
