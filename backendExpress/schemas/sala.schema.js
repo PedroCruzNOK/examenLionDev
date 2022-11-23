623ffb14ad27675fd43a3fe91f46a7e3a195cc3e
Pedro Cruz <cruzrodriguezpedroj@gmail.com>
2022-11-22 18:15:01 -0600

    Frontend: se agrega funcionalidad para actualizar sala

--- backendExpress/schemas/sala.schema.js
+++ backendExpress/schemas/sala.schema.js
@@ -6,23 +6,39 @@ const ubicacion = joi.string();
 const capacidad = joi.number().integer();
 const identificador = joi.string();
 const estado = joi.string();
+const fechainicial = joi.date();
+const fechafinal = joi.date();
+const solicitante = joi.string();
 
 
 const createSalaSchema = joi.object({
-    sala: sala.required(),
-    descripcion: descripcion.required(),
-    ubicacion: ubicacion.required(),
-    capacidad: capacidad.required(),
-    identificador: identificador.required(),
-    estado: estado.required(),
+  sala: sala.required(),
+  descripcion: descripcion.allow(),
+  ubicacion: ubicacion.allow(),
+  capacidad: capacidad.allow(),
+  identificador: identificador.allow(),
+  estado: estado.allow(),
+  fechainicial: fechainicial.allow(),
+  fechafinal: fechafinal.allow(),
+  solicitante: solicitante.allow(),
+
 });
 
 const updateSalaSchema = joi.object({
-    sala: sala.required(),
+  id: id.allow(),
+  sala: sala.allow(),
+  descripcion: descripcion.allow(),
+  ubicacion: ubicacion.allow(),
+  capacidad: capacidad.allow(),
+  identificador: identificador.allow(),
+  estado: estado.allow(),
+  fechainicial: fechainicial.allow(),
+  fechafinal: fechafinal.allow(),
+  solicitante: solicitante.allow(),
+
 
 
 })
-
 const getSalaSchema= joi.object({id:id.required(),});
 
 module.exports = {createSalaSchema, updateSalaSchema, getSalaSchema}
